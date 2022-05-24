979f1aa5ae88dd8a4ebd4aa35e5b799a0ff8f57a
hyejjun <hjlee9725@gmail.com>
2022-05-24 16:21:51 +0900

    ADD : login id pw

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,14 @@
 import React from "react";
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div>
+      login
+      <ul>
+        <li>id</li>
+        <li>pw</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Login;
